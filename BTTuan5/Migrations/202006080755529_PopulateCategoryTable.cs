1e2093b1e3a7ca5cadbbb2c1f3832a7e6c7ff7ce
Administrator <Administrator@PC210>
2020-06-08 15:26:53 +0700

    Tuan5

--- BTTuan5/Migrations/202006080755529_PopulateCategoryTable.cs
+++ BTTuan5/Migrations/202006080755529_PopulateCategoryTable.cs
@@ -7,42 +7,14 @@ namespace BTTuan5.Migrations
     {
         public override void Up()
         {
-            CreateTable(
-                "dbo.Categories",
-                c => new
-                    {
-                        Id = c.Byte(nullable: false),
-                        Name = c.String(nullable: false, maxLength: 255),
-                    })
-                .PrimaryKey(t => t.Id);
-            
-            CreateTable(
-                "dbo.Courses",
-                c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        LecturerId = c.String(nullable: false, maxLength: 128),
-                        Place = c.String(nullable: false, maxLength: 255),
-                        DateTime = c.DateTime(nullable: false),
-                        CatrgoryId = c.Byte(nullable: false),
-                        Category_Id = c.Byte(),
-                    })
-                .PrimaryKey(t => t.Id)
-                .ForeignKey("dbo.Categories", t => t.Category_Id)
-                .ForeignKey("dbo.AspNetUsers", t => t.LecturerId, cascadeDelete: true)
-                .Index(t => t.LecturerId)
-                .Index(t => t.Category_Id);
-            
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (1, 'Development')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (2, 'Business')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (3, 'Marketing')");   
         }
         
         public override void Down()
         {
-            DropForeignKey("dbo.Courses", "LecturerId", "dbo.AspNetUsers");
-            DropForeignKey("dbo.Courses", "Category_Id", "dbo.Categories");
-            DropIndex("dbo.Courses", new[] { "Category_Id" });
-            DropIndex("dbo.Courses", new[] { "LecturerId" });
-            DropTable("dbo.Courses");
-            DropTable("dbo.Categories");
+            
         }
     }
 }
