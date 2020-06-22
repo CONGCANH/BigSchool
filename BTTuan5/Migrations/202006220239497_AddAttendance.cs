04b15b52cce81a82d8c9cf352a0f22be650536e8
vo canh <congcanh16091999@gmail.com>
2020-06-22 09:51:23 +0700

    update

---
+++ BTTuan5/Migrations/202006220239497_AddAttendance.cs
@@ -0,0 +1,53 @@
+namespace BTTuan5.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddAttendance : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Attendances",
+                c => new
+                    {
+                        CourseId = c.Int(nullable: false),
+                        AttendeeId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.CourseId, t.AttendeeId })
+                .ForeignKey("dbo.AspNetUsers", t => t.AttendeeId, cascadeDelete: true)
+                .ForeignKey("dbo.Courses", t => t.CourseId)
+                .Index(t => t.CourseId)
+                .Index(t => t.AttendeeId);
+            
+            CreateTable(
+                "dbo.Followings",
+                c => new
+                    {
+                        FollowerId = c.String(nullable: false, maxLength: 128),
+                        FolloweeId = c.String(nullable: false, maxLength: 128),
+                        IsCanceled = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.FollowerId, t.FolloweeId })
+                .ForeignKey("dbo.AspNetUsers", t => t.FolloweeId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.FollowerId, cascadeDelete: true)
+                .Index(t => t.FollowerId)
+                .Index(t => t.FolloweeId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Followings", "FollowerId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Followings", "FolloweeId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Attendances", "CourseId", "dbo.Courses");
+            DropForeignKey("dbo.Attendances", "AttendeeId", "dbo.AspNetUsers");
+            DropIndex("dbo.Followings", new[] { "FolloweeId" });
+            DropIndex("dbo.Followings", new[] { "FollowerId" });
+            DropIndex("dbo.Attendances", new[] { "AttendeeId" });
+            DropIndex("dbo.Attendances", new[] { "CourseId" });
+            DropTable("dbo.Followings");
+            DropTable("dbo.Attendances");
+        }
+    }
+}
